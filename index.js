69f4af76be135ab113f798235ca1b2555db26c41
mattlusty <mattlusty123@gmail.com>
2024-01-19 21:26:54 +0000

    Implemented inquirer logic

--- index.js
+++ index.js
@@ -2,12 +2,92 @@ const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
+// const fs = require("fs");
 
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
+const team = [];
 
-// const render = require("./src/page-template.js");
+const memberThing = {
+  Manager: "officeNumber",
+  Engineer: "gitHub",
+  Intern: "school",
+};
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+const getQuestions = (type) => [
+  {
+    type: "input",
+    message: type + " name",
+    name: "name",
+  },
+  {
+    type: "input",
+    message: type + " id",
+    name: "id",
+  },
+  {
+    type: "input",
+    message: type + " email",
+    name: "email",
+  },
+  {
+    type: "input",
+    message: type + " " + memberThing[type],
+    name: "thing",
+  },
+];
+
+const nextActionQuestion = {
+  type: "list",
+  message: "Next Action",
+  name: "action",
+  choices: ["Add Engineer", "Add Intern", "Finish"],
+};
+
+inquirer.prompt(getQuestions("Manager")).then(({ name, id, email, officeNumber }) => {
+  console.log("================");
+  team.push(new Manager(name, id, email, officeNumber));
+
+  inquirer.prompt(nextActionQuestion).then(({ action }) => {
+    doAction(action);
+  });
+});
+
+function doAction(action) {
+  console.log("================");
+  switch (action) {
+    case "Add Engineer":
+      askFor("Engineer");
+      break;
+    case "Add Intern":
+      askFor("Intern");
+      break;
+    case "Finish":
+      finish("engineer");
+      break;
+  }
+}
+
+function askFor(type) {
+  inquirer.prompt(getQuestions(type)).then(({ name, id, email, thing }) => {
+    console.log("================");
+    switch (type) {
+      case "Manager":
+        team.push(new Manager(name, id, email, thing));
+        break;
+      case "Intern":
+        team.push(new Manager(name, id, email, thing));
+        break;
+    }
+    inquirer.prompt(nextActionQuestion).then(({ action }) => {
+      doAction(action);
+    });
+  });
+}
+
+function finish() {
+  // const OUTPUT_DIR = path.resolve(__dirname, "output");
+  // const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+  // const render = require("./src/page-template.js");
+  console.log("FINISHED!");
+}
